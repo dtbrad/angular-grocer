174765a4904b3b580fa50620faecd8fbe58acaf0
danieltbrad <danieltbrad@gmail.com>
2017-02-01 19:59:52 -0800

    Add login/logout and incorporate $window service

--- js/MainController.js
+++ js/MainController.js
@@ -1,35 +1,47 @@
-function MainController($http) {
+function MainController($http, $window) {
 
   var ctrl = this
   ctrl.appName = "Angular Grocer App";
-  ctrl.token = window.localStorage.jwt
+  ctrl.token = $window.localStorage.jwt
+  ctrl.user = { name: "daniel", email: "danieltbrad@gmail.com", password: "password"}
 
   ctrl.signup = function () {
     $http.post('http://localhost:3000/signup', { user: ctrl.user })
     .then(function successCallback(response) {
       if (response.data.errors) { console.log(response.data.errors) }
       else {
-        console.log(response.data);
-        ctrl.token = response.data.jwt
-        window.localStorage['jwt'] = response.data.jwt;}
-        console.log("now logged in");
+        $window.localStorage['jwt'] = response.data.jwt;}
+        ctrl.token = response.data.jwt;
       });
   };
 
+  ctrl.login = function () {
+    $http.post('http://localhost:3000/login', { user: ctrl.user })
+    .then(function successCallback(response) {
+      $window.localStorage.jwt = response.data.jwt;
+      ctrl.token = response.data.jwt;
+    });
+  };
+
   ctrl.getProducts = function () {
-      token = window.localStorage.jwt
-      $http({
-              method: 'GET',
-              url: 'http://localhost:3000/products',
-              headers: {
-                'Authorization': token
-              }
-            })
-      .then(function successCallback(response) {
-        if (response.data.error) { alert(response.data.error.message) }
-        else ctrl.products = response.data
-      });
-    };
+    $http({
+            method: 'GET',
+            url: 'http://localhost:3000/products',
+            headers: {
+              'Authorization': $window.localStorage.jwt
+            }
+          })
+    .then(function successCallback(response) {
+      if (response.data.error) { alert(response.data.error.message) }
+      else ctrl.products = response.data
+    });
+  };
+
+  ctrl.logout = function(){
+    delete window.localStorage.jwt;
+    delete ctrl.token
+    delete ctrl.products;
+  }
 
 }
 
