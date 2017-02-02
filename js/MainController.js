a627d92ece58eac0d4ea57ec5fcc49b87af40096
danieltbrad <danieltbrad@gmail.com>
2017-02-01 16:48:29 -0800

    Add method to register with server and store JWT

--- js/MainController.js
+++ js/MainController.js
@@ -1,7 +1,20 @@
-function MainController() {
+function MainController($http) {
 
   var ctrl = this
   ctrl.appName = "Angular Grocer App";
+  ctrl.token = window.localStorage.jwt
+
+  ctrl.signup = function () {
+    $http.post('http://localhost:3000/signup', { user: ctrl.user })
+    .then(function successCallback(response) {
+      if (response.data.errors) { console.log(response.data.errors) }
+      else {
+        console.log(response.data);
+        ctrl.token = response.data.jwt
+        window.localStorage['jwt'] = response.data.jwt;}
+        console.log("now logged in");
+      });
+  };
 
 }
 
